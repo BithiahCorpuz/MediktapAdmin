8fcdc748ca9a0c09897fb98f8d0a0aef9e38bc46
Bithiah Corpuz <bithiahcorpuz@gmail.com>
2022-06-26 20:57:21 +0800

    changes

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace MediktapAdmin.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.2.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,8 @@ namespace MediktapAdmin.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Server=localhost\\SQLEXPRESS01;Database=NewDB;Trusted_Connection=True;")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-N1RG1OM\\SQLEXPRESS;Initial Catalog=NewDB;Integrated Security=" +
+            "True")]
         public string ConnectionString {
             get {
                 return ((string)(this["ConnectionString"]));
